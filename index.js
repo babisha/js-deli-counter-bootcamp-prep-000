53aeaa0493f0b51b79d78e15ad826474606c91ca
Babisha Manandhar <babisha.manandhar@gmail.com>
2018-02-12 19:43:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-var katzDeli = [];
-
-function takeANumber(line, name) {
-  line.push(name) 
-  
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
-}
-
-function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!"
-  } 
-    return `Currently serving ${line.shift()}`;
-  
-}
-
 function currentLine(line) {
   if (!line.length) {
     return "The line is currently empty."
   }
-  const numbersAndNames = [];
-  
+
+  const numbersAndNames = []
+
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
-  return `The line is currently: ${numbersAndNames.join(',')}`
-}
 
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
 
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
